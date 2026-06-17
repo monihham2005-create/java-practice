72df671fce36731a677ff562a16efcd5decf7352
Monihha <monihha.m2005@gmail.com>
2026-06-18 00:55:54 +0530

    collection java

---
+++ thurs.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class thurs{
+    public static void main(String[] args){
+       ArrayList a=new ArrayList();
+       a.add(890);
+       a.add(900);
+       a.add("hello");
+       
+       System.out.println(a);
+       
+       
+       a.add("world");
+       
+       System.out.println(a);
+       a.remove(1);
+       System.out.println(a);
+       a.add(1,1234567890);
+       System.out.println(a);
+       a.set(0,"welcome");
+       System.out.println(a);
+    }
+}
